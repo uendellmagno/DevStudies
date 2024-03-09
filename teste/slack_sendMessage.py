b082d7b5f1588d6da57a3110b0c3a1633083a7de
Uendell Magno <uendellmagno@gmail.com>
2024-03-09 16:50:05 -0300

    Be happy

--- teste/slack_sendMessage.py
+++ teste/slack_sendMessage.py
@@ -24,28 +24,97 @@ print(token)
 
 client = slack.WebClient(token=token)
 
+# a = 'Teste'
+# b = 'de'
+# c = 'vczvcxcxvczVVVVVVVVVVlongmessagerczvczxvczxvxvcxzvc'
+# d = 'YEY!'
+#
+# _structure = ''
+# for i in range(80):
+#     phrase = f'\n{a} - {b} - {c} - {d}'
+#     print('phrase:', phrase)
+#     if len(_structure + phrase) <= 4000:
+#         _structure += phrase
+#         print('structure is: ', _structure)
+#     else:
+#         _structure += '...break_here...'
+#         print('structure is: ', _structure)
+#     if len(_structure) > 0:
+#         _structure += '...break_here...'
+#
+# messages = _structure.split('...break_here...')
+# print('MESSAGES SPLIT:', messages)
+# header = '_Summary_ of Partial update - *DAILY:*\n\n'
+# footer = f"*Total Clients:* XX | *Accounts Failed:* XX"
+#
+# client.chat_postMessage(channel='U06GSVDJRCP', text=header)
+# for message in messages:
+#     client.chat_postMessage(channel='U06GSVDJRCP', text=f"```{message}```")
+#     print('message:', message)
+# client.chat_postMessage(channel='U06GSVDJRCP', text=footer)
+
 a = 'Teste'
 b = 'de'
-c = 'mensagem'
+c = 'vczvcxcxvczVVVVVVVVVVlongmessagerczvczxvczxvxvcxzvc'
 d = 'YEY!'
 
-msg = 'Tst of summary: \n\n```'
-for i in range(500):
-    msg += f'\n{a} - {b} - {c} - {d}'
-msg += '```\n\n Teste de fim'
+messages = []
+current_message = ''
+for i in range(150):
+    phrase = f'\n{a} - {b} - {c} - {d}'
 
-fetch_api = requests.get('https://api.exchangerate-api.com/v4/latest/USD').json()
-exchangerate_api = fetch_api['rates']['BRL']
+    # Check if current message + phrase fits within the limit
+    if len(current_message + phrase) <= 4000:
+        current_message += phrase
+    else:
+        # If overflow, add current message as a block and start a new one
+        messages.append(f"```{current_message}```")
+        current_message = phrase
+
+# Add the last message block even if it's less than 4000 characters
+if current_message:
+    messages.append(f"```{current_message}```")
+
+header = '_Summary_ of Partial update - *DAILY:*\n\n'
+footer = f"*Total Clients:* XX | *Accounts Failed:* XX"
+
+client.chat_postMessage(channel='U06GSVDJRCP', text=header)
+for message in messages:
+    client.chat_postMessage(channel='U06GSVDJRCP', text=message)  # Send each message as a code block
+    print('message:', message)
+client.chat_postMessage(channel='U06GSVDJRCP', text=footer)
 
 
-fetch_api = requests.get('https://economia.awesomeapi.com.br/json/last/USD-BRL').json()
-awesome_api = fetch_api['USDBRL']['high']
-print(f"Exchange rate: {awesome_api}")
 
-with open('exchange_rate.json', 'w') as f:
-    f.write(f'{fetch_api}')
-    f = open('exchange_rate.json', 'w')
-    f.close()
+#
+# # Check if the message exceeds Slack API character limit (maximum is 4000 characters)
+# if counter <= 80:
+#     # Send the result as a single code block
+#     fm = f"Tst of summary: \n\n```{msg}```\n\n Teste de fim"
+#     client.chat_postMessage(channel='U06GSVDJRCP', text=fm)
+# else:
+#     # Split the message into chunks if it exceeds the character limit
+#     chunk_size = 4000  # Adjust as needed
+#     result_chunks = [result_code_block[i:i + chunk_size] for i in range(0, len(result_code_block), chunk_size)]
+#
+#     client.chat_postMessage(channel='U06GSVDJRCP', text=f'{lele}')
+#     # Send each chunk as a separate message
+#     for chunk in result_chunks:
+#         client.chat_postMessage(channel='U06GSVDJRCP', text=f'```{chunk}```')
+#     client.chat_postMessage(channel='U06GSVDJRCP', text=f'Teste de fim')
+
+# fetch_api = requests.get('https://api.exchangerate-api.com/v4/latest/USD').json()
+# exchangerate_api = fetch_api['rates']['BRL']
+#
+#
+# fetch_api = requests.get('https://economia.awesomeapi.com.br/json/last/USD-BRL').json()
+# awesome_api = fetch_api['USDBRL']['high']
+# print(f"Exchange rate: {awesome_api}")
+#
+# with open('exchange_rate.json', 'w') as f:
+#     f.write(f'{fetch_api}')
+#     f = open('exchange_rate.json', 'w')
+#     f.close()
 # client.chat_postMessage(channel='U06GSVDJRCP', text=msg)
 # user_list()
 
